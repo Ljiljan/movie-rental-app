58b7fa1141cdb04b565e95b34a80a6dc5cb45fc2
FG <fikretgogic@gmail.com>
2018-04-13 11:04:29 +0200

    Add project files.

---
+++ movie-rental-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(movie_rental_app.Startup))]
+namespace movie_rental_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
