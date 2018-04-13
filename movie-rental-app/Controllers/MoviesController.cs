0585358683bf6646b77c0d58b9af3e861cf38c00
FG <fikretgogic@gmail.com>
2018-04-13 13:34:36 +0200

    Added Movies controller and View

---
+++ movie-rental-app/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using movie_rental_app.Models;
+
+namespace movie_rental_app.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var Movies = new Movie() {
+                Name = "Goodfather"
+            };
+
+            return View(Movies);
+        }
+    }
+}
