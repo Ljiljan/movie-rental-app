7d8c30bfbe3abd96de5f2735fab84ea86144320c
F. Gogic <fikretgogic@gmail.com>
2018-04-15 00:36:58 +0200

    Added bootstrap 4, updated navigation and stuff

--- movie-rental-app/Models/Movie.cs
+++ movie-rental-app/Models/Movie.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace movie_rental_app.Models
 {
-    public class Movie{
+    public class Movie {
         public int Id { get; set; }
         public string Name { get; set; }
     }
