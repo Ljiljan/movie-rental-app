d7a464a2f71b34409e36483ecf8d1d21830f3e97
FG <fikretgogic@gmail.com>
2018-04-30 08:42:44 +0200

    Added AutoMapper to customers API class

--- movie-rental-app/Global.asax.cs
+++ movie-rental-app/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using movie_rental_app.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,7 @@ namespace movie_rental_app
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(conf => conf.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
